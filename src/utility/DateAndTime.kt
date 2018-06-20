66c1e7180219b1dd0ae3bbd42e277138aa7a6080
oniX0n <oniX0nlp@gmail.com>
2018-06-20 19:19:11 +0200

    Added time zone and put it into calender

--- src/utility/DateAndTime.kt
+++ src/utility/DateAndTime.kt
@@ -12,6 +12,9 @@ class DateAndTime {
             val format = SimpleDateFormat("yyyyMMddHHmmss")
             val calendar = Calendar.getInstance()
             calendar.time = format.parse(dateString)
+
+            val timeZoneBerlin = TimeZone.getTimeZone("Europe/Berlin")
+            calendar.timeZone = timeZoneBerlin
             return calendar
         }
     }
