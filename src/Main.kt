845ce8f9970f864b2291d2544f608be2a38149ef
oniX0n <oniX0nlp@gmail.com>
2018-06-21 22:24:31 +0200

    Added countdown and time with instant and duration

--- src/Main.kt
+++ src/Main.kt
@@ -3,4 +3,6 @@ import information.Launch
 fun main(args: Array<String>) {
     val launch = Launch(1494)
     println(launch.getName())
+    println(launch.getTimeNet())
+    println(launch.getTimeUntilString())
 }
--- src/information/Launch.kt
+++ src/information/Launch.kt
@@ -2,6 +2,8 @@ package information
 
 import jsonUtility.JsonFromUrl
 import org.json.JSONObject
+import java.time.Duration
+import java.time.Instant
 
 
 class Launch constructor(id : Int) {
@@ -16,6 +18,13 @@ class Launch constructor(id : Int) {
 
     private val name = jsonMasterObject.getString("name")
     private val id = jsonMasterObject.getInt("id")
+    private val timeNet: Instant = Instant.ofEpochSecond(
+            jsonMasterObject.getInt("netstamp").toLong()
+    )
+
+    fun getTimeNet(): Instant {
+        return timeNet
+    }
 
     fun getName(): String {
         return name
@@ -23,5 +32,19 @@ class Launch constructor(id : Int) {
 
     fun getId(): Int {
         return id
+    }
+
+    fun getTimeUntilDuration(): Duration{
+        val timeNow = Instant.now()
+        return Duration.between(timeNow, timeNet)
+    }
+
+    fun getTimeUntilString(): String{
+        val duration = getTimeUntilDuration()
+        val days = (duration.toDays()).toString()
+        val hours = (duration.toHours() - duration.toDays() * 24).toString()
+        val minutes = (duration.toMinutes() - duration.toHours() * 60).toString()
+        val seconds = (duration.toMillis() / 1000 - duration.toMinutes() * 60).toString()
+        return "$days : $hours : $minutes : $seconds "
     }
 }
