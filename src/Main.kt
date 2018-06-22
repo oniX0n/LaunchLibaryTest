10525e733c30a6b5c7341aa2965bf7818b9374c9
oniX0n <oniX0nlp@gmail.com>
2018-06-22 17:26:06 +0200

    Removed getters in Launch and made vars public. Removed countdown string

--- src/Main.kt
+++ src/Main.kt
@@ -2,9 +2,6 @@ import information.Launch
 
 fun main(args: Array<String>) {
     val launch = Launch(1518)
-    println(launch.getName())
-    println(launch.getTimeNet())
-    println(launch.getTimeWindowStart())
-    println(launch.getTimeWindowEnd())
-    println(launch.getTimeUntilString())
+    println(launch.name)
+    println(launch.timeNet)
 }
--- src/information/Launch.kt
+++ src/information/Launch.kt
@@ -16,50 +16,23 @@ class Launch constructor(id : Int) {
         jsonMasterObject = JSONObject(jsonCompleteArray[0].toString())
     }
 
-    private val name = jsonMasterObject.getString("name")
-    private val id = jsonMasterObject.getInt("id")
-    private val timeNet: Instant = Instant.ofEpochSecond(
+    val name = jsonMasterObject.getString("name")
+
+    val id = jsonMasterObject.getInt("id")
+
+    val timeNet: Instant = Instant.ofEpochSecond(
             jsonMasterObject.getInt("netstamp").toLong()
     )
-    private val timeWindowStart: Instant = Instant.ofEpochSecond(
+
+    val timeWindowStart: Instant = Instant.ofEpochSecond(
             jsonMasterObject.getInt("wsstamp").toLong()
     )
-    private val timeWindowsEnd: Instant = Instant.ofEpochSecond(
+
+    val timeWindowsEnd: Instant = Instant.ofEpochSecond(
             jsonMasterObject.getInt("westamp").toLong()
     )
 
-
-    fun getTimeNet(): Instant {
-        return timeNet
-    }
-
-    fun getTimeWindowStart(): Instant {
-        return  timeWindowStart
-    }
-
-    fun getTimeWindowEnd(): Instant {
-        return timeWindowsEnd
-    }
-
-    fun getName(): String {
-        return name
-    }
-
-    fun getId(): Int {
-        return id
-    }
-
-    fun getTimeUntilDuration(): Duration{
-        val timeNow = Instant.now()
-        return Duration.between(timeNow, timeNet)
-    }
-
-    fun getTimeUntilString(): String{
-        val duration = getTimeUntilDuration()
-        val days = (duration.toDays()).toString()
-        val hours = (duration.toHours() - duration.toDays() * 24).toString()
-        val minutes = (duration.toMinutes() - duration.toHours() * 60).toString()
-        val seconds = (duration.toMillis() / 1000 - duration.toMinutes() * 60).toString()
-        return "$days : $hours : $minutes : $seconds "
-    }
+    val timeUntilDuration = Duration.between(
+            Instant.now(), timeNet
+    )
 }
