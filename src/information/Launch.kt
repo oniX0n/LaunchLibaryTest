c6c80dfca3789428efa09a48755f8f80eaca83d9
oniX0n <oniX0nlp@gmail.com>
2018-06-22 17:54:17 +0200

    Created countdown subclass and prepared making it

--- src/information/Launch.kt
+++ src/information/Launch.kt
@@ -16,6 +16,8 @@ class Launch constructor(id : Int) {
         jsonMasterObject = JSONObject(jsonCompleteArray[0].toString())
     }
 
+
+
     val name = jsonMasterObject.getString("name")
 
     val id = jsonMasterObject.getInt("id")
@@ -32,7 +34,15 @@ class Launch constructor(id : Int) {
             jsonMasterObject.getInt("westamp").toLong()
     )
 
-    val timeUntilDuration = Duration.between(
-            Instant.now(), timeNet
-    )
+    val countdown = Countdown(timeNet)
+
+
+
+    class Countdown constructor(private val launchInstant: Instant){
+
+        private fun duration(): Duration{
+            return Duration.between(Instant.now(), launchInstant)
+        }
+    }
 }
+
