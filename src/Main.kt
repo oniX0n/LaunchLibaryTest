3c689ef651db93244c4147c6841277eb57959d79
oniX0n <oniX0nlp@gmail.com>
2018-07-10 18:02:22 +0200

    Deleted created classes and altered Main.kz to show different launch

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,7 @@
 import information.Launch
 
 fun main(args: Array<String>) {
-    val launch = Launch(1518)
+    val launch = Launch(1391)
     println(launch.name)
     println(launch.timeNet)
     println(launch.countdown.string())
