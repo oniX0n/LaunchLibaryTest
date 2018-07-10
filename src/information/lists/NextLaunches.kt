b040b014c05f7a2c388d5bdfee5fc727cd980071
oniX0n <oniX0nlp@gmail.com>
2018-07-10 18:22:57 +0200

    Added launchArray to provide launches

--- src/information/lists/NextLaunches.kt
+++ src/information/lists/NextLaunches.kt
@@ -1,16 +1,21 @@
 package information.lists
 
+import information.Launch
 import jsonUtility.JsonFromUrl
 import org.json.JSONObject
-import java.util.*
 
 class NextLaunches constructor(number: Int){
+    private val launchArray: Array<Launch>
+
     init {
         val jsonSourceObject = JsonFromUrl.jsonObjectFromUrl("https://launchlibrary.net/1.4/launch/next/$number")
         val jsonSourceArray = jsonSourceObject.getJSONArray("launches")
         val idArray = Array(number, {
             i -> JSONObject(jsonSourceArray[i].toString()).getInt("id")
         })
-        println(Arrays.toString(idArray))
+
+        launchArray = Array(number, {
+            i -> Launch(idArray[i])
+        })
     }
 }
